516a90134753a70f53b0390828ddc5cd568f6e71
Bradley Davis <bradley.davis@ntlworld.com>
2022-12-13 21:52:44 +0000

    Christmas Tree Application

---
+++ ChristmasTree/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int treeSize;
+            bool validInput = false;
+            bool running = true;
+            while (running)
+            {
+                do
+                {
+                    Console.Write("How large would you like your tree? ");
+                    userInput = Console.ReadLine();
+
+                    if (!int.TryParse(userInput, out treeSize))
+                    {
+                        validInput = false;
+                        Console.WriteLine("You need to enter a number!");
+                    }
+                    else
+                    {
+                        if (treeSize < 2)
+                        {
+                            Console.WriteLine("Tree too small!");
+                            validInput = false;
+                        }
+                        else if (treeSize > 110)
+                        {
+                            Console.WriteLine("PBCAK");
+                            validInput = false;
+                        }
+                        else
+                        {
+                            validInput = true;
+                        }
+                    }
+                } while (!validInput);
+
+                Console.WriteLine();
+
+                TreeCreator myTree = new TreeCreator();
+                myTree.CreateTree(treeSize);
+
+                Console.Write("Create another tree? Y = yes: ");
+                userInput = Console.ReadKey().KeyChar.ToString();
+                Console.WriteLine();
+                running = (userInput == "y" || userInput == "Y");
+            }
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
---
+++ ChristmasTree/TreeCreator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasTree
+{
+    class TreeCreator
+    {
+        Random numberGen;
+        int rows;
+        public TreeCreator()
+        {
+            this.numberGen = new Random();
+            this.rows = 0;
+        }
+        public void CreateTree(int rows)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            this.rows = rows;
+            PrintTree();
+            PrintPlantPot();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private void PrintTree()
+        {
+            int spaces = this.rows;
+
+            for (int depth = 0; depth < this.rows; depth++)
+            {
+                for (int whitespace = 0; whitespace < spaces; whitespace++)
+                {
+                    Console.Write(" ");
+                }
+                spaces--;
+
+                for (int width = 0; width <= depth; width++)
+                {
+                    if (numberGen.Next() % 8 == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    Console.Write("*");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                for (int width = 0; width <= depth; width++)
+                {
+                    if (numberGen.Next() % 8 == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    Console.Write("*");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void PrintPlantPot()
+        {
+            int spacesRequired = this.rows - 3;
+            PrintSpaces(spacesRequired);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("  **  ");
+            PrintSpaces(spacesRequired);
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("******");
+            PrintSpaces(spacesRequired);
+            Console.WriteLine("******");
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+
+        private void PrintSpaces(int spacesRequired)
+        {
+            for (int ppSpaces = 0; ppSpaces <= spacesRequired; ppSpaces++)
+            {
+                Console.Write(" ");
+            }
+        }
+    }
+}
